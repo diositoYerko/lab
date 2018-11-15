875be23b2e61bf79218ed20af960e4dc4aa4064f
Drkzzz <34901982+Drkzzz@users.noreply.github.com>
2018-11-15 11:40:47 -0300

    lab update 15 nov

--- lab3.c
+++ lab3.c
@@ -10,6 +10,7 @@ char lineaA[7],lineaB[7],lineaC[7],lineaD[7],lineaE[7],lineaF[7],lineaG[7],linea
 
 void fArchivo()
 {
+int num1,num2,num3;
 
 FILE *archivo;
 
@@ -36,12 +37,12 @@ char nombre_archivo[100]; int ciclo=0; int i; char textoExtraido[N][N];
 
 /*Copiamos los datos del archivo a un arreglo por linea*/
     /*Linea 1*/
-    fscanf(archivo, "%d" , &ttablero);
-    printf("%d",ttablero);
+    fscanf(archivo, "%d" , &num1);
+    printf("%d",num1);
     salto;
     /*Linea 2*/
-    fscanf(archivo, "%d", &nembarcaciones);
-    printf("%d",nembarcaciones);
+    fscanf(archivo, "%d", &num2);
+    printf("%d",num2);
     salto;
     /*Linea 3*/
     fscanf(archivo,"%s",lineaA);
@@ -84,40 +85,122 @@ char nombre_archivo[100]; int ciclo=0; int i; char textoExtraido[N][N];
 		printf("%c ",lineaH[i]);
 	salto;
     /*Linea 11*/
-    fscanf(archivo, "%d", &tdisponibles);
-    printf("%d",tdisponibles);
+    fscanf(archivo, "%d", &num3);
+    printf("%d",num3);
     salto;
 /*FIN*/
+ttablero=num1;
+nembarcaciones=num2;
+tdisponibles=num3;
 
 /*Cerramos el archivo*/
 fclose(archivo);
 /*FIN*/
 
+salto;salto;salto;
 }
 
 void fTablero()
 {
 int i,j,k;
-
-//erroneo=-1
+/*Tamaño Barcos*/
 //goleta=2
 //fragata=3
 //corbeta=4
 
 /*Copiamos los datos a la matriz*/
 
-/*Linea A*/
-for(i=0;i<N;i++;)
-{
-	for(j=0;j<N;j++)
-	{
-		for(k=0;k<N;j++)
-		{
-			if(lineaA[k]=
-		}
-	}
+    /*Linea A*/
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaA[k];
+            }
+        }
+    }
+    /*Linea B*/
+    for(i=1;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaB[k];
+            }
+        }
+    }
+    /*Linea C*/
+    for(i=2;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaC[k];
+            }
+        }
+    }
+    /*Linea D*/
+    for(i=3;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaD[k];
+            }
+        }
+    }
+    /*Linea E*/
+    for(i=4;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaE[k];
+            }
+        }
+    }
+    /*Linea F*/
+    for(i=5;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaF[k];
+            }
+        }
+    }
+    /*Linea G*/
+    for(i=6;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaG[k];
+            }
+        }
+    }
+    /*Linea H*/
+    for(i=7;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            for(k=0;k<7;k++)
+            {
+                tablero[i][j]=lineaH[k];
+            }
+        }
+    }
+/*Imprimimos tablero*/
+ImprimeTablero(tablero,ttablero);
 }
-
 
 
 
@@ -130,7 +213,7 @@ for(i=0;i<N;i++;)
 
 
 /*FIN*/
-}
+
 
 void main()
 {
