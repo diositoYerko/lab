d08301c59318e47db1dcc932bcd7e160467821a7
Drkzzz <joaquin.escobarr@gmail.com>
2018-11-18 19:04:58 -0300

    git actualizado 18-11-2018

--- lab3.c
+++ lab3.c
@@ -1,14 +1,27 @@
+/*
+    *Nombres:Joaquin Escobar , Yerko Tolosa
+    *Asignatura: Fundamentos de Programacion
+    *Profesores:Irene Zuccar , Marcelo Baeza
+    *Fecha entrega:Domingo 25 de noviembre 2018
+*/
+
+
+/*Headers*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "funcionesParaImprimir.h"
-#define salto printf("\n")
+/*FIN*/
 
+/*Macros propios*/
+#define salto printf("\n")
+#define and &&
+#define or ||
+/*FIN*/
 
 int ttablero; int nembarcaciones; int tdisponibles; int tablero[N][N];
 char lineaA[7],lineaB[7],lineaC[7],lineaD[7],lineaE[7],lineaF[7],lineaG[7],lineaH[7];
 
-void fArchivo()
+void fArchivo(void)
 {
 int num1,num2,num3;
 
@@ -30,6 +43,7 @@ char nombre_archivo[100]; int ciclo=0; int i; char textoExtraido[N][N];
 		else
         {
          ciclo++;
+        /*evaluamos break; pero no esta dentro de la materia vista en clases. (cumple la misma función que ciclo++)*/
 		}
 
 	}
@@ -38,75 +52,44 @@ char nombre_archivo[100]; int ciclo=0; int i; char textoExtraido[N][N];
 /*Copiamos los datos del archivo a un arreglo por linea*/
     /*Linea 1*/
     fscanf(archivo, "%d" , &num1);
-    printf("%d",num1);
-    salto;
     /*Linea 2*/
     fscanf(archivo, "%d", &num2);
-    printf("%d",num2);
-    salto;
     /*Linea 3*/
     fscanf(archivo,"%s",lineaA);
-    for(i=0;i<=7;i++)
-		printf("%c ",lineaA[i]);
-	salto;
 	/*Linea 4*/
 	fscanf(archivo,"%s",lineaB);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaB[i]);
-	salto;
 	/*Linea 5*/
 	fscanf(archivo,"%s",lineaC);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaC[i]);
-	salto;
 	/*Linea 6*/
 	fscanf(archivo,"%s",lineaD);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaD[i]);
-	salto;
 	/*Linea 7*/
 	fscanf(archivo,"%s",lineaE);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaE[i]);
-	salto;
 	/*Linea 8*/
 	fscanf(archivo,"%s",lineaF);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaF[i]);
-	salto;
 	/*Linea 9*/
 	fscanf(archivo,"%s",lineaG);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaG[i]);
-	salto;
 	/*Linea 10*/
 	fscanf(archivo,"%s",lineaH);
-	for(i=0;i<=7;i++)
-		printf("%c ",lineaH[i]);
-	salto;
     /*Linea 11*/
     fscanf(archivo, "%d", &num3);
-    printf("%d",num3);
-    salto;
 /*FIN*/
+
+/*Asignamos valores de nuestras variables auxiliares*/
 ttablero=num1;
 nembarcaciones=num2;
 tdisponibles=num3;
+/*FIN*/
 
 /*Cerramos el archivo*/
 fclose(archivo);
 /*FIN*/
 
-salto;salto;salto;
+LimpiaPantalla();
 }
 
-void fTablero()
+void rellenarTablero(void)
 {
 int i,j,k;
-/*Tamaño Barcos*/
-//goleta=2
-//fragata=3
-//corbeta=4
 
 /*Copiamos los datos a la matriz*/
 
@@ -198,26 +181,49 @@ int i,j,k;
             }
         }
     }
-/*Imprimimos tablero*/
-ImprimeTablero(tablero,ttablero);
+/*FIN*/
 }
 
+void fJuego(void)
+{
+    char fila; int f; int c; int ciclo;
+    /*Comienza el juego*/
+    //while(tdisponibles>0)
+    //{
+    ImprimeTablero(tablero,ttablero); salto; salto;
+    printf("Total barcos: %d - Tiros Disponibles: %d",nembarcaciones,tdisponibles); salto; salto;
+    printf("Ingrese fila: ");
+    scanf(" %c", &fila);
 
+        /*Identificamos numero de fila a partir del caracter ingresado*/
+        if( (fila=='A')or(fila=='a') )
+            f=0;
+        if( (fila=='B')or(fila=='b') )
+            f=1;
+        if( (fila=='C')or(fila=='c') )
+            f=2;
+        if( (fila=='D')or(fila=='d') )
+            f=3;
+        if( (fila=='E')or(fila=='e') )
+            f=4;
+        if( (fila=='F')or(fila=='f') )
+            f=5;
+        if( (fila=='G')or(fila=='g') )
+            f=6;
+        if( (fila=='H')or(fila=='h') )
+            f=7;
+        /*FIN*/
+    printf("Ingrese columna: ");
+    scanf("%d",&c);
+        /*Preguntamos en nuestro tablero si la posicion coincide con una parte de una nave*/
 
+    //}
+}
 
-
-
-
-
-
-
-
-/*FIN*/
-
-
-void main()
+void main(void)
 {
 	fArchivo();
-	fTablero();
+	rellenarTablero();
+	fJuego();
 }
 
